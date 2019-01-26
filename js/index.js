803762182c873341799f411708c90f6f918dace5
Yazan Samara <yazan@Khalids-MacBook-Pro.local>
2019-01-26 13:35:49 +0400

    redirect

--- js/index.js
+++ js/index.js
@@ -33,6 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        window.location('https://www.syrialist.net');
+        window.location.href = 'https://www.syrialist.net';
     }
 };
