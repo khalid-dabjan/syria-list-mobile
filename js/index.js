01ea9a67172dcbc6bdb94028df83e2477bb93ed6
Khalid Dabjan <khalid.dabjan@gmail.com>
2019-01-26 18:32:52 +0400

    splash

--- js/index.js
+++ js/index.js
@@ -34,6 +34,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
         // window.location.href = 'https://www.syrialist.net';
-        window.open('https://www.syrialist.net', '_self', 'location=no');
+        // window.open('https://www.syrialist.net', '_self', 'location=no');
     }
 };
